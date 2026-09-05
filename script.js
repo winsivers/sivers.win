dc916825ed80b74ac5bca5909bb06696e34d2f71
Win Sivers <katong@posteo.net>
2026-09-05 13:44:01 +1200

    background

--- script.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener('scroll', () => {
-  document.body.style.setProperty('--scroll-x', `${window.scrollY * 0.5}px`);
-  document.body.style.setProperty('--scroll-y', `${window.scrollY * 0.5}px`);
-});
